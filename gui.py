7f2d2ff24d24ea33711e2a40ad9402991ae1b8fe
Ahmed Gado <ahmedgado0100@gmail.com>
2023-10-30 16:46:42 +0200

    Bug: fix a bug to make it work on linux
    the window would appear smaller and sometime the alpha value was not
    used

--- gui.py
+++ gui.py
@@ -4,77 +4,80 @@ from text_snipper import TextSnipper
 
 # Fix wrong resolution on Windows
 from platform import system
+
 if system() == "Windows":
     from ctypes import windll
+
     try:
-        windll.shcore.SetProcessDpiAwareness(2) # if your windows version >= 8.1
+        windll.shcore.SetProcessDpiAwareness(2)  # if your windows version >= 8.1
     except:
-        windll.user32.SetProcessDPIAware() # win 8.0 or less 
+        windll.user32.SetProcessDPIAware()  # win 8.0 or less
+
 
 class App:
-    
     BACKGROUND_COLOR: str = "gray10"
-    
+
     def __init__(self, args: list[str]) -> None:
-        self.master = tk.Tk() 
+        self.master = tk.Tk()
         self.master.title("Text Snipper")
-        
+
         self.master.attributes("-fullscreen", True)
-        self.master.configure(background= App.BACKGROUND_COLOR)
-        self.master.resizable(False, False)
+        self.master.configure(background=App.BACKGROUND_COLOR)
+
+        self.master.wait_visibility()
         self.master.attributes("-alpha", 0.3)
-         
+
         self.master.bind("<Escape>", lambda _: self.master.destroy())
 
-        self.canvas = tk.Canvas(self.master, bg= App.BACKGROUND_COLOR)
-        self.canvas.pack(fill= tk.BOTH, expand= True)
-        
+        self.canvas = tk.Canvas(self.master, bg=App.BACKGROUND_COLOR)
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+
         self.canvas.bind("<Button-1>", self.on_mouse_press)
         self.canvas.bind("<B1-Motion>", self.on_mouse_motion)
         self.canvas.bind("<ButtonRelease-1>", self.on_mouse_release)
-        
+
         self.startX: int = -1
         self.startY: int = -1
         self.endX: int = -1
         self.endY: int = -1
         self.text_snipper: TextSnipper = TextSnipper(args[1], args[2:])
-        
+
     def run(self) -> None:
         self.master.mainloop()
-        
+
     @property
     def box(self) -> tuple[int, int, int, int]:
         return (self.startX, self.startY, self.endX, self.endY)
-    
+
     def order_box(self) -> None:
         if self.startX > self.endX:
             self.startX, self.endX = self.endX, self.startX
         if self.startY > self.endY:
             self.startY, self.endY = self.endY, self.startY
-        
+
     def on_mouse_press(self, event: tk.Event) -> None:
         self.startX = event.x
         self.startY = event.y
-        
+
     def on_mouse_motion(self, event: tk.Event) -> None:
         self.endX = event.x
         self.endY = event.y
-        
+
         self.draw_rect()
-        
+
     def on_mouse_release(self, event: tk.Event) -> None:
         self.endX = event.x
         self.endY = event.y
-         
+
         self.master.destroy()
-        
+
         self.order_box()
-        
+
         if self.startX == self.endX or self.startY == self.endY:
             self.text_snipper.getTextFromImage(None)
         else:
-            self.text_snipper.getTextFromImage(self.box)    
-        
+            self.text_snipper.getTextFromImage(self.box)
+
     def draw_rect(self) -> None:
         self.canvas.delete("all")
-        self.canvas.create_rectangle(self.box, fill= "gray90", outline= "white")
+        self.canvas.create_rectangle(self.box, fill="gray90", outline="white")
--- text_snipper.py
+++ text_snipper.py
@@ -9,56 +9,73 @@ import pyperclip
 
 from typing import Callable
 
+
 class TextSnipper:
-    
-    TesseractPath: str = r'C:\My Program Files\Tesseract-OCR\tesseract.exe' # path to tesseract.exe
-    
+    TesseractPath: str = (
+        # r"C:\My Program Files\Tesseract-OCR\tesseract.exe"  # path to tesseract.exe
+        "/usr/bin/tesseract"
+    )
+
     def __init__(self, mode: str, preprocess_modes: list[str]) -> None:
         self.image: Image.Image | None = None
         self.image_array: np.ndarray | None = None
         self.text: str | None = None
-        
-        self.kernel = np.ones((5,5),np.uint8)
 
-        self.preprocess: dict[str, Callable] = {"-g": self.get_grayscale, "-r": self.remove_noise, "-t": self.thresholding, "-d": self.dilate, "-e": self.erode, "-o": self.opening, "-c": self.canny, "-s": self.skew}
-        self.preprocess_modes: list[str] = preprocess_modes if preprocess_modes[0] != "" else []
-        
-        self.models: dict[str, Callable] = {"easyocr": self.easyocr_mode, "tesseract": self.tesseract_mode}    
+        self.kernel = np.ones((5, 5), np.uint8)
+
+        self.preprocess: dict[str, Callable] = {
+            "-g": self.get_grayscale,
+            "-r": self.remove_noise,
+            "-t": self.thresholding,
+            "-d": self.dilate,
+            "-e": self.erode,
+            "-o": self.opening,
+            "-c": self.canny,
+            "-s": self.skew,
+        }
+        self.preprocess_modes: list[str] = (
+            preprocess_modes if preprocess_modes[0] != "" else []
+        )
+
+        self.models: dict[str, Callable] = {
+            "easyocr": self.easyocr_mode,
+            "tesseract": self.tesseract_mode,
+        }
         self.model: str = mode
-    
+
     def getTextFromImage(self, box: tuple[int, int, int, int] | None) -> None:
         print("Getting image...")
         self.getImage(box)
-        
+
         print("Preprocessing image...")
         self.preprocessing()
-        
+
         print("Reading text from image...")
-        self.models[self.model]() 
-        
+        self.models[self.model]()
+
         print("Copying text to clipboard...")
         self.copyTextToClipboard()
-            
+
         print("DONE!")
-        
+
     def getImage(self, box: tuple[int, int, int, int] | None) -> None:
-        self.image = ImageGrab.grab(bbox= box)
+        self.image = ImageGrab.grab(bbox=box)
         self.image_array = np.array(self.image)
-    
+
     def copyTextToClipboard(self) -> None:
         assert self.text is not None
         pyperclip.copy(self.text)
-    
+
     def easyocr_mode(self) -> None:
-        reader: easyocr.Reader = easyocr.Reader(['en'])
+        reader: easyocr.Reader = easyocr.Reader(["en"])
 
         # detail = 0 returns only the text
-        self.text = "\n".join(reader.readtext(self.image_array, detail= 0)) # type: ignore
+        self.text = "\n".join(reader.readtext(self.image_array, detail=0))  # type: ignore
 
     def tesseract_mode(self) -> None:
         pytesseract.pytesseract.tesseract_cmd = self.TesseractPath
 
-        self.text = pytesseract.image_to_string(self.image_array, lang='eng')
+        self.text = pytesseract.image_to_string(self.image_array, lang="eng")
 
     def preprocessing(self) -> None:
         assert self.image_array is not None
@@ -73,34 +90,36 @@ class TextSnipper:
     # noise removal
     def remove_noise(self) -> np.ndarray:
         assert self.image_array is not None
-        return cv2.medianBlur(self.image_array,5)
-    
-    #thresholding    
+        return cv2.medianBlur(self.image_array, 5)
+
+    # thresholding
     def thresholding(self) -> np.ndarray:
         assert self.image_array is not None
-        return cv2.threshold(self.image_array, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    
-    #dilation
+        return cv2.threshold(
+            self.image_array, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+        )[1]
+
+    # dilation
     def dilate(self) -> np.ndarray:
         assert self.image_array is not None
-        return cv2.dilate(self.image_array, self.kernel, iterations = 1)
-        
-    #erosion
+        return cv2.dilate(self.image_array, self.kernel, iterations=1)
+
+    # erosion
     def erode(self) -> np.ndarray:
         assert self.image_array is not None
-        return cv2.erode(self.image_array, self.kernel, iterations = 1)
+        return cv2.erode(self.image_array, self.kernel, iterations=1)
 
-    #opening - erosion followed by dilation
+    # opening - erosion followed by dilation
     def opening(self) -> np.ndarray:
         assert self.image_array is not None
         return cv2.morphologyEx(self.image_array, cv2.MORPH_OPEN, self.kernel)
 
-    #canny edge detection
+    # canny edge detection
     def canny(self) -> np.ndarray:
         assert self.image_array is not None
         return cv2.Canny(self.image_array, 100, 200)
 
-    #skew correction
+    # skew correction
     def skew(self) -> np.ndarray:
         assert self.image_array is not None
         coords = np.column_stack(np.where(self.image_array > 0))
@@ -112,10 +131,16 @@ class TextSnipper:
         (h, w) = self.image_array.shape[:2]
         center = (w // 2, h // 2)
         M = cv2.getRotationMatrix2D(center, angle, 1.0)
-        rotated = cv2.warpAffine(self.image_array, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+        rotated = cv2.warpAffine(
+            self.image_array,
+            M,
+            (w, h),
+            flags=cv2.INTER_CUBIC,
+            borderMode=cv2.BORDER_REPLICATE,
+        )
         return rotated
 
-    #template matching
+    # template matching
     def match_template(self, template: cv2.Mat) -> cv2.Mat:
         assert self.image_array is not None
         return cv2.matchTemplate(self.image_array, template, cv2.TM_CCOEFF_NORMED)
